36ecfecd82781cd73c527d121a8007a864058730
wallaceleonardo <wallace.tolentino@gmail.com>
2024-08-29 09:28:41 -0300

    Colocando interatividade com JavaScript

---
+++ index.js
@@ -0,0 +1,8 @@
+// declaração de variáveis
+var elementosDuvida = document.querySelectorAll('.duvida')
+
+elementosDuvida.forEach(function (duvida) {
+    duvida.addEventListener('click', function() {
+        duvida.classList.toggle('ativa')
+    })
+})
